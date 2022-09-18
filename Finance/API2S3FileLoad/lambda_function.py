bdda251326d4bc0ec5d1c5d923261985e79d2d3a
AbhishekSingh1180 <Abhishek11801529@gmail.com>
2022-09-19 00:56:20 +0530

    add company field

--- Finance/API2S3FileLoad/lambda_function.py
+++ Finance/API2S3FileLoad/lambda_function.py
@@ -5,19 +5,19 @@ def lambda_handler(event, context):
 
     try:
         top_stock = {
-                        'Apple':'AAPL',
-                        'Microsoft':'MSFT',
-                        'Google':'GOOG',
-                        'Amazon':'AMZN',
-                        'Tesla':'TSLA',
-                        '':'BRK-A',
-                        '':'UNH',
-                        '':'JNJ',
-                        '':'V',
-                        '':'META'
+                        "APPLE INC": "AAPL",
+                        "MICROSOFT CORPORATION": "MSFT",
+                        "ALPHABET INC": "GOOG",
+                        "AMAZON.COM INC": "AMZN",
+                        "TESLA INC": "TSLA",
+                        "BERKSHIRE HATHAWAY INC": "BRK-A",
+                        "UNITEDHEALTH GROUP INC": "UNH",
+                        "JOHNSON & JOHNSON": "JNJ",
+                        "VISA INC": "V",
+                        "META INC": "META"
                     }
 
-        data = api.get_data("MSFT")
+        data = api.get_data(top_stock["MICROSOFT CORPORATION"])
         path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data/")
 
         bucket = None
@@ -25,7 +25,7 @@ def lambda_handler(event, context):
             config = json.load(f)
             bucket = config["bucket"]
 
-        S3.transfer(data, path, bucket)
+        S3.transfer(data, path, bucket, top_stock)
         
         return {
         'statusCode': 200,
--- Finance/API2S3FileLoad/module/S3.py
+++ Finance/API2S3FileLoad/module/S3.py
@@ -1,31 +1,34 @@
-import datetime as dt
-import os
-import re
-import boto3
+import datetime as dt, json, boto3, re
 
-def data_cleaning(data):
+
+def data_cleaning(data, top_stock):
 
     regex = r"\"\d.\s"
     test_str = data
     subst = "\""
     dataCleaned = re.sub(regex, subst, test_str, 0, re.MULTILINE)
 
+    dataCleaned = json.loads(dataCleaned)
+    dataCleaned["Meta Data"]["Company Name"] =  list(top_stock.keys())[list(top_stock.values()).index(dataCleaned["Meta Data"]["Symbol"])]
+    dataCleaned = json.dumps(dataCleaned, indent=4)
+
     return dataCleaned if dataCleaned else 'Failed'
 
 
-def transfer(data, path, bucket):
+def transfer(data, path, bucket, top_stock):
 
-    data = data_cleaning(data)
+    data = data_cleaning(data, top_stock)
 
     if data == 'Failed':
         raise Exception("Data cleaning failed")
 
     INDTimeStamp = dt.datetime.now(dt.timezone(dt.timedelta(hours=5, minutes=30))).strftime("%Y%m%d")
     stock = eval(data)['Meta Data']['Symbol']
+    company = eval(data)['Meta Data']['Company Name']
  
     S3_connect = boto3.client('s3')
     try:
-        S3_connect.put_object(Body=data, Bucket=bucket, Key="{}/{}/Timeseries_{}_{}.json".format(stock,INDTimeStamp, stock, INDTimeStamp))
+        S3_connect.put_object(Body=data, Bucket=bucket, Key="{}/{}/Timeseries_{}_{}.json".format(company,INDTimeStamp, stock, INDTimeStamp))
         return True
     except:
         raise  Exception("File transfer Failed")
